8688134fec904691e8ee3ec21b45f8f46b9f8afc
Yohan Graterol <yohangraterol92@gmail.com>
2018-11-10 22:40:18 -0500

    Fix mined blocks

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ var Block = new Schema(
     "transactionsRoot": String,
     "stateRoot": String,
     "receiptRoot": String,
-    "miner": String,
+    "miner": {type: String, lowercase: true},
     "difficulty": String,
     "totalDifficulty": String,
     "size": Number,
