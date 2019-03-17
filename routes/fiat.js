d6bca95ebf34929e61d7ae8e23bec7d0a0a9ca59
Yohan Graterol <yohangraterol92@gmail.com>
2019-03-17 18:10:13 -0500

    Refactor fiat.js filters.js and richlist.js

--- routes/fiat.js
+++ routes/fiat.js
@@ -1,33 +1,32 @@
-var http = require('http');
-var etherUnits = require(__lib + "etherUnits.js")
+const http = require('http');
 
-module.exports = function(req, res) {
-  var addr = req.body.addr;
-  if (typeof addr !== "undefined")
-    addr = addr.toLowerCase();
-  else 
-    res.sendStatus(400);
+const etherUnits = require(`${__lib}etherUnits.js`);
 
-  var options = {
+module.exports = function (req, res) {
+  let { addr } = req.body;
+  if (typeof addr !== 'undefined') addr = addr.toLowerCase();
+  else { res.sendStatus(400); }
+
+  const options = {
     host: 'api.blockcypher.com',
     port: '80',
-    path: '/v1/eth/main/addrs/' + addr + '/balance',
-    method: 'GET'
+    path: `/v1/eth/main/addrs/${addr}/balance`,
+    method: 'GET',
   };
 
-  var balance = 0;
-  http.request(options, function(bcRes) {
-    bcRes.on('data', function (data) {
+  let balance = 0;
+  http.request(options, (bcRes) => {
+    bcRes.on('data', (data) => {
       try {
         balance = JSON.parse(data).balance;
-        balance = etherUnits.toEther( balance, "wei");
+        balance = etherUnits.toEther(balance, 'wei');
       } catch (e) {
-        console.error("BC err, probably invalid addr");
+        console.error('BC err, probably invalid addr');
       }
-      res.write(JSON.stringify({"balance": balance}));
+      res.write(JSON.stringify({ 'balance': balance }));
       res.end();
-    })
+    });
   }).end();
 
-}
+};
 
